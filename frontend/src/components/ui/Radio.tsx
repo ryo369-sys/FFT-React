2a4054383a0caa84b6b36b2a0666afc44824a7cd
ryo369-sys <shimuraryo117@gmail.com>
2026-05-24 17:45:19 +0900

    RadioBottom-add

--- frontend/src/components/ui/Radio.tsx
+++ frontend/src/components/ui/Radio.tsx
@@ -1,9 +1,47 @@
-const Radio = () => {
+import React, { useState } from "react";
+
+export const RadioExample = () => {
+  // 選択された値を保持するステート
+  const [selectValue, setSelectedValue] = useState<string>("FFT");
+
+  // 値の変更を処理する関数
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(e.target.value);
+  };
+
   return (
     <div>
-      Radio
+      <label>
+        <input
+          type="radio"
+          name="sound"
+          value="FFT"
+          checked={selectValue === "FFT"}
+          onChange={handleChange}
+        />
+        FFT
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sound"
+          value="Wave"
+          checked={selectValue === "Wave"}
+          onChange={handleChange}
+        />
+        Wave
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sound"
+          value="Spectrum"
+          checked={selectValue === "Spectrum"}
+          onChange={handleChange}
+        />
+        Wave
+      </label>
+      <p>選択された形式: {selectValue}</p>
     </div>
-  )
-}
-
-export default Radio
+  );
+};
