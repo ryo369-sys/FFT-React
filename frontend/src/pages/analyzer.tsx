7c09d628d7e84236e386f0268f435ee136a36cb7
ryo369-sys <shimuraryo117@gmail.com>
2026-05-29 17:45:43 +0900

    file-drag-add

--- frontend/src/pages/analyzer.tsx
+++ frontend/src/pages/analyzer.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
 import { SoundButton } from '../components/ui/read_file';
+import { FileUpload } from '../components/ui/read-sound';
 
 const Analyzer = () => {
   return (
     <div>
       <h1>analyzer</h1>
+      <FileUpload/>
       <SoundButton/>
     </div>
   );
