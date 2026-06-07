80be2cb410e48a62ec4c989da9770917c361f7d5
ryo369-sys <shimuraryo117@gmail.com>
2026-06-07 17:46:33 +0900

    check-cof

--- python/app/routers/fft.py
+++ python/app/routers/fft.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from fastapi import UploadFile, File
+from app.fft.analyze import analyze
 
 router = APIRouter(
     prefix="/fft",
@@ -16,4 +18,17 @@ def fft_check():
     return {
         "frequency": [1,2],
         "amplitude": [2,3]
+    }
+
+@router.post("/fft/upload")
+async def upload_audio(
+    audio_file: UploadFile = File(...)
+):
+    sample = analyze()
+    print(audio_file.filename)
+    contents = await audio_file.read()
+    print(len(contents))
+
+    return {
+        "filename": audio_file.filename
     }
