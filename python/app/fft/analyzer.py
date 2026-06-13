f09b45171be27368adbd89859a5b35724b3fbf04
ryo369-sys <shimuraryo117@gmail.com>
2026-06-13 17:40:12 +0900

    complete-sep

--- python/app/fft/analyzer.py
+++ python/app/fft/analyzer.py
@@ -5,3 +5,16 @@ def analyze(signal):
     freq = np.fft.fftfreq(len(signal))
 
     return freq, np.abs(fft)
+
+def spectrum(signal,sample_rate):
+    fft = np.fft.fft(signal)
+
+    freqs = np.fft.fftfreq(
+        len(signal),
+        d=1/sample_rate
+    )
+
+    shifted_fft = np.fft.fftshift(fft)
+    shifted_freqs = np.fft.fftshift(freqs)
+
+    return shifted_freqs, np.abs(shifted_fft)
