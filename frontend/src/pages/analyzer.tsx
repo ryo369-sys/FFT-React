59092414e61a6ecd10f730a1048c29695812e6d3
ryo369-sys <shimuraryo117@gmail.com>
2026-06-13 17:42:48 +0900

    complete-tsx

--- frontend/src/pages/analyzer.tsx
+++ frontend/src/pages/analyzer.tsx
@@ -3,13 +3,19 @@ import { FileUpload } from '../components/ui/read-sound';
 import { FFTChart }  from '../components/charts/FFTChart';
 import { useEffect } from 'react'
 import { AnalyzeButton } from '../components/ui/AnalyzeButtom';
+import { Spectrum } from '../components/ui/Spectrum';
 import React,{useState } from 'react';
+import { SpectrumChart } from '../components/charts/SpectrumChart';
 
 type WavePoint = {
   time: number
   amplitude: number
 }
 
+type SpectrumPoint = {
+  frequency: number
+  amplitude: number
+}
 
 export const Analyzer = () =>  {
 
@@ -19,27 +25,29 @@ export const Analyzer = () =>  {
   const [waveData, setWaveData] =
   useState<WavePoint[]>([])
 
+  const [spectrumData, setSpectrumData] =
+  useState<SpectrumPoint[]>([])
+
   useEffect(() => {
     fetch('http://localhost:8000/fft/send-wave')
       .then((res) => res.json())
       .then((data) => console.log(data))
   }, [])
 
-
-
-//const waveChartData = data.time.map(
-//  (time: number, index: number) => ({
-//    time,
-//    amplitude: data.amplitude[index]
-//  })
-//)
+    useEffect(() => {
+    fetch('http://localhost:8000/fft/spectrum')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, [])
 
   return (
     <div>
       <h1>analyzer</h1>
       <FileUpload onFileSelect = {setSelectedFile}/>
       <AnalyzeButton file = {file}  onResult={setWaveData}/>
+      <Spectrum file = {file}  onResult={setSpectrumData}/>
       <FFTChart data = {waveData}/>
+      <SpectrumChart data = {spectrumData}/>
       <div>
         <SoundButton/>
       </div>
